0808c0f08f4524566694adc1df76741512789cd0
Matt Anderson <matt.anderson.inf@gmail.com>
2019-12-19 23:56:11 -0500

    test
    test

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 /* app.js */
 
+// use ajax to call the random user generator API
+$.ajax({
+    url: 'https://randomuser.me/api/',
+    dataType: 'json',
+    success: function(data) {
+      console.log(data);
+    }
+  });
 
+
+//generate user content into containers
