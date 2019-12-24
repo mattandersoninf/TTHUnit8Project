193121a256e9da1f44bf6a715a1e21249b743147
Matt Anderson <matt.anderson.inf@gmail.com>
2019-12-24 03:05:25 -0500

    Random user employee_directory html complete
    Random user employee_directory html complete

--- js/app.js
+++ js/app.js
@@ -1,45 +1,49 @@
 /* app.js */
 
-const userContainer = $('.user-container');
-
-const randomUserUrl = "https://randomuser.me/api/?results=12&inc=picture,name,email,location";
-
 // use fetch api to set up th e
-$.ready(function(url){
+$(document).ready(function(){
+
+  const url = "https://randomuser.me/api/?results=12&inc=picture,name,email,location,cell,dob";
 
   // call the fetch api to grab information from the provided url
   // format the response into json
   // disect the json information
+  // IMPORTANT: fetch returns objects
   fetch(url)
-  .then((response) => (response.json()))
+  .then((response) => response.json())
   .then(function(data){
-    console.log(data);
+
+    data.results.forEach(user => {
+
+      console.log(user);
+
+      // build the user-container
+
+      userInfoHTML = `
+      <div class="user-container grid-section grid">
+        <div class="user-img-container">
+          <img src="${user.picture.medium}" class="img-rounded" alt="${user.email}"></img>
+        </div>
+        <div class="user-txt-container">
+          <span class="user-name-container">${user.name.first + " " + user.name.last}</span>
+          <span class="user-email-container">${user.email}</span>
+          <span class="user-city-container">${user.location.city}</span>
+        </div>          
+      </div>
+      `;
+
+      var currentUser = $('.users-container').append(userInfoHTML);
+
+      
+    });
+    
+
   });
 
+
 });
 
-/*
-// use ajax to call the random user generator API
-$.ajax({
-  url: 'https://randomuser.me/api/?inc=picture,name,email,location',
-  dataType: 'json',
-  success: function(data) {
-    console.log(data);
-  }
-});
-*/
+// form user div container
 
-/*
-//generate user content into containers
-function getRandomEmployeeData(url){
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url, true);
-  xhr.onreadystatechange = function(){
-    if(xhr.readyState === 4 && xhr.status === 200){
-      let data = JSON.parse(xhr.responseText);
-      // to be replace with returning the JSON information into a new employee object
-      data.map(p => console.log);
-    }
-  };
-}
-*/
+
+// provide classes 
