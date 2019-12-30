aa77a28420f60644ef73ca8c872b84cab20788b0
Matt Anderson <matt.anderson.inf@gmail.com>
2019-12-30 01:26:08 -0500

    Prev and Next buttons work
    Prev and Next buttons work

--- js/app.js
+++ js/app.js
@@ -21,16 +21,21 @@ var visibleEmployeesList = [];
 // use fetch api to set up the employee containers
 $(document).ready(function(){
 
+  // the randomuser generator API site
   const url = "https://randomuser.me/api/?results=12&inc=picture,name,email,location,cell,dob";
 
+  // hide the modal element from the start (this could have been done in the html as well)
   modal.hide();
 
+  // this counter is for populating the employee-container with an unique class name
   let i = 0;
 
-  // call the fetch api to grab information from the provided url
-  // format the response into json
-  // disect the json information
+  /* FETCH API PROMISE
+  // 1. Call the fetch api to grab information from the provided url
+  // 2. Format the response into json
+  // 3. Disect the json information and sort it's results into the employeeInfoHTML.
   // IMPORTANT: fetch returns objects
+  */
   fetch(url)
   .then((response) => response.json())
   .then(function(data){
@@ -88,7 +93,12 @@ $(document).ready(function(){
     // apply an eventlistener to every employee-container
     var employeeContainers = document.querySelector(".employees-container").querySelectorAll('[class*="employee-container"]');
 
-    //
+    /* EMPLOYEE CONTAINER EVENT LISTENER
+    // Clicking any of the employee containers triggers the following sequence of events
+    // 1. Populate the visibleEmployeeList array with the visible (style.display != "none;") employee-container classes.
+    // 2. Show the modal content that corresponds to the employee-container class that was clicked,
+    // 3. Show the modal. 
+    */
     employeeContainers.forEach(employeeContainer =>{
       
       employeeContainer.addEventListener("click", function(){
@@ -96,7 +106,7 @@ $(document).ready(function(){
         // get the visible employee-container element's class numbers and store them in visibleEmployeesList
         for(j = 0; j < employeesContainer.find('[class*="employee-container-"]:visible').length; j++){
           
-          visibleEmployeesList.push(employeesContainer.find('[class*="employee-container-"]:visible')[j]);
+          visibleEmployeesList.push(employeesContainer.find('[class*="employee-container-"]:visible')[j].classList[0]);
 
         }
 
@@ -112,15 +122,77 @@ $(document).ready(function(){
 
   });
 
-  // add event listener for the modal close button
+  /* MODAL CLOSE EVENT LISTENER
+  // 1. Hide all of the employee-modal-text-containers.
+  // 2. Empty the visibleEmployeesList by setting it's length to 0.
+  // 3. Hide the modal.
+  */
   $(".modal-close").click(function(){
     
+    // hide all of the modal employee content
     $(".modal-employee-content").find(".employee-modal-text-container").hide();
 
+    // clear the visibleEmployeeList
+    visibleEmployeesList.length = 0;
+
+    // hide the modal
     modal.hide();
 
   });
 
+  /* PREVIOUS BUTTON EVENT LISTENER
+  // 1. Get the employee-container class that is currently visible
+  // 2. Get the employee-container class's index in the visibleEmployeeList
+  // 3. Hide the currently visible container
+  // 3. Decrement the index
+  // 4. Check if the index falls into the range of the visibleEmployeeList
+  // 4.a. if it doesn't reset the index to the length of the visibleEmployeeList
+  // 5. Show the employee-container corresponding to the new index in the visibleEmployeeList
+  */
+  $(".prev-container").click(function(){
+
+    let tempVisibleModalEmployee = $('.modal-employee-content [class*="employee-container-"]:visible')[0].classList[1];
+
+    let currIndex = visibleEmployeesList.indexOf(tempVisibleModalEmployee);
+
+    $('.modal-employee-content [class*="employee-container-"]:visible').hide();
+
+    currIndex--;
+
+    if(currIndex < 0){
+      currIndex = visibleEmployeesList.length -1;
+    }
+
+    $(".modal-employee-content ."+ visibleEmployeesList[currIndex]).show();
+
+  });
+
+  /* NEXT BUTTON EVENT LISTENER
+  // 1. Get the employee-container class that is currently visible
+  // 2. Get the employee-container class's index in the visibleEmployeeList
+  // 3. Hide the currently visible container
+  // 3. Increment the index
+  // 4. Check if the index falls into the range of the visibleEmployeeList
+  // 4.a. if it doesn't reset the index to the length of the visibleEmployeeList
+  // 5. Show the employee-container corresponding to the new index in the visibleEmployeeList
+  */
+  $(".next-container").click(function(){
+
+    let tempVisibleModalEmployee = $('.modal-employee-content [class*="employee-container-"]:visible')[0].classList[1];
+
+    let currIndex = visibleEmployeesList.indexOf(tempVisibleModalEmployee);
+
+    $('.modal-employee-content [class*="employee-container-"]:visible').hide();
+
+    currIndex++;
+
+    if(currIndex >= visibleEmployeesList.length){
+      currIndex = 0;
+    }
+
+    $(".modal-employee-content ."+ visibleEmployeesList[currIndex]).show();
+
+  });
 
 });
 
